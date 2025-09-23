94301a86f978414c0e9dc607944f3e65564ddd5d
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-09-23 17:27:00 +0300

    feat: enhance onboarding experience with Lottie animations and updated UI
    - Updated onboarding screens to include Lottie animations for a more engaging user experience.
    - Replaced static text with CustomText components for better styling and consistency.
    - Improved navigation flow with new Pressable components for skipping and proceeding through onboarding.
    - Updated package.json to use newer versions of lottie-ios and lottie-react-native.

--- app/onboarding/final.tsx
+++ app/onboarding/final.tsx
@@ -1,22 +1,63 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 import React from 'react';
-import { useAuthStore } from '@/utils/authStore';
 import { router } from 'expo-router';
+import Lottie from 'lottie-react-native';
+import animation from '@/assets/lottie/animation2.json';
+import CustomText from '@/components/ui/CustomText';
+import { ArrowRight } from 'lucide-react-native';
+import { useAuthStore } from '@/utils/authStore';
 
-export default function FinalOnboarding() {
+const FinalOnboarding = () => {
+  const { width } = useWindowDimensions();
   const { completeOnboarding } = useAuthStore();
   return (
-    <View>
-      <Text>FinalOnboarding</Text>
-      <Button
-        title="Finish"
-        onPress={() => {
-          completeOnboarding();
-          router.push('/create-account');
-        }}
+    <SafeAreaView className="flex-1 items-center justify-center bg-[#d9d9d9]">
+      <CustomText text="Let's get you started" className="text-2xl" />
+      <Lottie
+        source={animation}
+        autoPlay
+        loop
+        style={{ width: width * 0.8, height: width * 0.8 }}
       />
-    </View>
+      <CustomText
+        text="Create an account a user account and begin paying for goods and services at insalely low fees and insstant speeds"
+        className="text- px-4 text-sm leading-relaxed"
+      />
+
+      <View className="absolute bottom-10 px-10">
+        <View className="mb-4 flex flex-row items-center justify-center gap-2">
+          <Pressable
+            className="h-2 w-2 rounded-full bg-gray-500"
+            onPress={() => {
+              router.push('/onboarding');
+            }}
+          />
+          <View className="h-4 w-4 rounded-full bg-[#191919]" />
+        </View>
+        <View className="flex w-full flex-row items-center justify-between">
+          <CustomText text="Skip" className="text-gray-500" />
+          <Pressable
+            className="flex flex-row items-center justify-center gap-1 rounded-full bg-[#191919] p-2"
+            onPress={() => {
+              completeOnboarding();
+            }}>
+            <CustomText text="Create Account" className="text-[#d9d9d9]" />
+            <ArrowRight className="size-6" color="#d9d9d9" />
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default FinalOnboarding;
 
 const styles = StyleSheet.create({});
--- app/onboarding/index.tsx
+++ app/onboarding/index.tsx
@@ -1,20 +1,57 @@
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 import React from 'react';
 import { router } from 'expo-router';
 import Lottie from 'lottie-react-native';
 import animation from '@/assets/lottie/animation1.json';
+import CustomText from '@/components/ui/CustomText';
+import { ArrowRight } from 'lucide-react-native';
 
 const FirstOnboarding = () => {
+  const { width } = useWindowDimensions();
   return (
-    <SafeAreaView className="flex-1 items-center justify-center">
-      <Text>FirstOnboarding</Text>
-      <Lottie source={animation} autoPlay loop style={{ width: 200, height: 200 }} />
-      <Button
-        title="Next"
-        onPress={() => {
-          router.push('/onboarding/final');
-        }}
+    <SafeAreaView className="flex-1 items-center justify-center bg-[#d9d9d9]">
+      <CustomText text="Welcome to the Orcus App" className="text-2xl" />
+      <Lottie
+        source={animation}
+        autoPlay
+        loop
+        style={{ width: width * 0.8, height: width * 0.8 }}
       />
+      <CustomText
+        text="Pay instantly with crypto on the fastest, most
+          secure network. Your money, your way - with fees as low as 1 KSH"
+        className="text- px-4 text-sm leading-relaxed"
+      />
+
+      <View className="absolute bottom-10 px-10">
+        <View className="flex flex-row items-center justify-center gap-2">
+          <View className="h-4 w-4 rounded-full bg-[#191919]" />
+          <Pressable
+            className="h-2 w-2 rounded-full bg-gray-500"
+            onPress={() => {
+              router.push('/onboarding/final');
+            }}
+          />
+        </View>
+        <View className="flex w-full flex-row items-center justify-between">
+          <CustomText text="Skip" className="text-gray-500" />
+          <Pressable
+            className="flex items-center justify-center rounded-full bg-[#191919] p-2"
+            onPress={() => {
+              router.push('/onboarding/final');
+            }}>
+            <ArrowRight className="size-6" color="#d9d9d9" />
+          </Pressable>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
