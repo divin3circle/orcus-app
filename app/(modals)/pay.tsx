5b492a96f6c68e3bff80fb1c9be13228a119b8c5
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-09-26 13:29:36 +0300

    feat: enhance Pay modal with payment success feedback and transaction details
    - Added success animation and message display upon successful payment confirmation.
    - Implemented transaction details section showing Transaction ID, Shop ID, Merchant ID, Amount, Fee, and Date.
    - Improved user experience by providing clear feedback and options after payment completion.

--- app/(modals)/pay.tsx
+++ app/(modals)/pay.tsx
@@ -1,13 +1,27 @@
-import { Image, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 import React, { useEffect, useState } from 'react';
 import CustomText from '@/components/ui/CustomText';
 import CustomKeyboards from '@/components/ui/custom-keyboard';
 import { useAuthStore } from '@/utils/authStore';
+import Lottie from 'lottie-react-native';
+import animation from '@/assets/lottie/successAnimation.json';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useColorScheme } from 'nativewind';
 
 const Pay = () => {
   const { paymentID } = useAuthStore();
+  const { colorScheme } = useColorScheme();
   const [amount, setAmount] = useState('0');
   const [paymentId, setPaymentId] = useState('');
+  const [paymentSuccess, setPaymentSuccess] = useState(false);
 
   useEffect(() => {
     setPaymentId(paymentID);
@@ -40,8 +54,75 @@ const Pay = () => {
   };
 
   const handleConfirm = () => {
+    if (amount === '0') {
+      return;
+    }
     console.log('Confirming payment:', amount);
+    setPaymentSuccess(true);
   };
+
+  if (paymentSuccess) {
+    return (
+      <View className="flex justify-center p-4">
+        <View className="flex w-full items-center justify-center">
+          <Lottie source={animation} autoPlay loop={false} style={{ width: 200, height: 200 }} />
+          <CustomText text="Payment Successful" className="text-lg font-semibold" />
+          <Text
+            className="text-sm text-foreground/50"
+            style={{
+              fontFamily: 'Montserrat',
+            }}>
+            You payment of KES {amount} was successful
+          </Text>
+        </View>
+        <View className="mt-12">
+          <CustomText text="Transaction Details" className="text-lg font-semibold" />
+          <View className="mt-4 flex gap-4 rounded-xl border border-foreground/20 bg-input/30 p-4">
+            <View className="flex flex-row items-center justify-between">
+              <CustomText text="Transaction ID" className="font-semibold" />
+              <Text className="text-sm text-foreground/50">{paymentID}</Text>
+            </View>
+            <View className="flex flex-row items-center justify-between">
+              <CustomText text="Shop ID" className="font-semibold" />
+              <Text className="text-sm text-foreground/50">{paymentID}</Text>
+            </View>
+            <View className="flex flex-row items-center justify-between">
+              <CustomText text="Merchant ID" className="font-semibold" />
+              <Text className="text-sm text-foreground/50">{paymentID}</Text>
+            </View>
+            <View className="flex flex-row items-center justify-between">
+              <CustomText text="Amount" className="font-semibold" />
+              <Text className="text-sm text-foreground/50">KSH {amount}</Text>
+            </View>
+            <View className="flex flex-row items-center justify-between">
+              <CustomText text="Fee" className="font-semibold" />
+              <Text className="text-sm text-foreground/50">
+                KSH {(Number(amount) * 0.005).toFixed(2)}
+              </Text>
+            </View>
+            <View className="flex flex-row items-center justify-between">
+              <CustomText text="Date" className="font-semibold" />
+              <Text className="text-sm text-foreground/50">{new Date().toLocaleDateString()}</Text>
+            </View>
+          </View>
+          <View className="mt-8 flex flex-row items-center justify-between">
+            <Pressable className="flex w-[45%] flex-row items-center justify-center gap-2 rounded-xl bg-blue-500 p-4">
+              <CustomText text="Join Campaign" />
+            </Pressable>
+            <Pressable className="flex w-[45%] flex-row items-center justify-center gap-2 rounded-xl bg-foreground p-4">
+              <Text
+                className="text-sm font-semibold text-background"
+                style={{
+                  fontFamily: 'Montserrat',
+                }}>
+                Save Shop
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    );
+  }
 
   return (
     <View className="">
